2b3fa45789621c2db9de8f83b61ca1c06e30cb08
Aldwinny <aldwin0313@yahoo.com.ph>
2022-11-26 14:33:30 +0800

    Server -> Services

---
+++ services/api/test.php
@@ -0,0 +1,9 @@
+<?php
+
+include '../utils/token.php';
+
+$token =  "";
+
+echo json_encode(["message" => "No tests"]);
+// echo Token::getHeader($token);
+// echo json_decode(Token::getPayload($token), true)["access_level"];
